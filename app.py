4430dffa25e941afd3dcc321c9b40609aad57ead
APSwinner <ague_soria@yahoo.es>
2026-05-30 16:43:41 +0200

    fix:  misswritting

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from ui import AvatarWindow
 if __name__ == "__main__":
 
     app_id = "orion.macondo.avatar.1"
-    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModeID(app_id)
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(app_id)
     
     app = QApplication(sys.argv)
     app.setWindowIcon(QIcon("assets\icon.ico"))
